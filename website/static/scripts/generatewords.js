00b5beec042289a008d83eb45fd7f17e8943facc
lameburger <laneburgett01@gmail.com>
2024-06-12 12:25:44 -0500

    gifs

--- website/static/scripts/generatewords.js
+++ website/static/scripts/generatewords.js
@@ -1,15 +1,18 @@
 // List of random words
-const words = ['hello', 'thanks', 'iloveyou'];
+let words = ['burrito', 'hello', 'i love you', 'meet you', 'my', 'name', 'nice', 'thanks', 'time', 'what', 'where'];
 
-// Function to generate a random word
+// Function to generate a random word without repetition
 function generateRandomWord() {
     const randomIndex = Math.floor(Math.random() * words.length);
-    return words[randomIndex];
+    const word = words[randomIndex];
+    words.splice(randomIndex, 1); // Remove the selected word from the array
+    return word;
 }
 
 // Populate the word container with random words
 const wordContainer = document.querySelector('.wordcontainer .word');
-for (let i = 0; i < 5; i++) { // Populate 3 words
+for (let i = 0; i < 5; i++) { // Populate 5 words
+    if (words.length === 0) break; // Break if no more words are available
     const word = generateRandomWord();
     const spans = Array.from(word).map(letter => `<span>${letter}</span>`).join('');
     wordContainer.innerHTML += `<div>${spans}</div>`;
